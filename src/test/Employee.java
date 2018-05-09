383a73b9cbdf1f87e15ec6a9394299250684e5c7
hp1 <hp1@192.168.100.7>
2018-05-09 20:51:44 +0530

    story#2018

---
+++ src/test/Employee.java
@@ -0,0 +1,37 @@
+package test;
+
+public class Employee {
+	private int empId;
+	private String empName;
+	private double empSal;
+	
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public double getEmpSal() {
+		return empSal;
+	}
+	public void setEmpSal(double empSal) {
+		this.empSal = empSal;
+	}
+	
+	
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", empSal=" + empSal + "]";
+	}
+	
+	
+	
+	
+
+}
